105d77b616c3935ede3bd8112193c4570a10af71
Vitallya <vitaliy.petreev@gmail.com>
2024-11-08 06:55:36 +0300

    Added PromptVoice event (not vorking yet)

--- api/handlers/prompting.py
+++ api/handlers/prompting.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+from starlette.responses import Response
 
 from api.models import PromptTextModel, TextModel
 from api.status_messages import bot_not_found
@@ -6,14 +7,29 @@ from api.utils.checking import is_bot
 
 from api.bots_data import bots
 
+from rage.tts import ToMachineSpeech
+
 
 router = APIRouter()
 
+tts_model = ToMachineSpeech()
 
-@router.post("/prompt_text")
+
+@router.post("/prompttext")
 async def prompt_text(body: PromptTextModel) -> TextModel:
     if is_bot(body.bot_id):
         model_answer = await bots[body.bot_id](body.prompt)
         return TextModel(text=model_answer)
 
     return TextModel(text=bot_not_found, status=bot_not_found)
+
+
+@router.post("/promptvoice")
+async def prompt_voice(body: PromptTextModel):
+    if is_bot(body.bot_id):
+        model_answer = await bots[body.bot_id](body.prompt)
+        return Response(
+            content=tts_model.get_bytes(model_answer),
+            media_type="audio/wav"
+        )
+    return bot_not_found
